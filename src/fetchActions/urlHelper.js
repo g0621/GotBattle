8af7dbe4539fa82f92ded89b88f21b49a1e2f6f4
Gyan Vardhan <gyansingh1997@gmail.com>
2020-05-22 22:52:27 +0530

    new url

--- src/fetchActions/urlHelper.js
+++ src/fetchActions/urlHelper.js
@@ -1,2 +1,2 @@
-const ServerUrl = process.env.REACT_APP_BASE_URL ? 'http://localhost:5000/api/battle' : 'https://got-gyan-battle.herokuapp.com/api/battle';
+const ServerUrl = process.env.REACT_APP_BASE_URL ? 'http://localhost:5000/api/battle' : 'https://got-battle-gyan.herokuapp.com/api/battle';
 export default ServerUrl;
